01ac27e50fb88396d14ca298486d7e069d229806
Bhawana Kumari <bhawanangp@gmail.com>
2023-06-01 01:47:51 +0530

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebForm.Models;
+
+namespace WebForm.Controllers
+{
+	public class ValuesController : Controller
+	{
+		private static List<Values> valuesList = new List<Values>();
+		public IActionResult Index()
+		{
+			return View(valuesList);
+		}
+
+		[HttpPost]
+		public ActionResult AddValues(Values values)
+		{
+			valuesList.Add(values);
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ Models/Values.cs
@@ -0,0 +1,11 @@
+﻿namespace WebForm.Models
+{
+	public class Values
+	{
+		public decimal Quantity { get; set; }
+		public int Rate { get; set; }
+		public decimal Value => Quantity * Rate;
+
+		public decimal Total => Value;
+	}
+}
